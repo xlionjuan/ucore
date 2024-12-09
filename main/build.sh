5f97c95348dfca128ec26b482296cd9519b703d8
XLion <xlion@xlion.tw>
2024-12-10 00:38:53 +0800

    CachyOS Kernel

--- main/build.sh
+++ main/build.sh
@@ -18,3 +18,12 @@ dnf5 install -y screen ntpd-rs sudo-rs vim htop wget
 ## Use ntpd-rs to replace chronyd
 systemctl disable chronyd
 systemctl enable ntpd-rs
+
+# CachyOS Kernel
+dnf5 -y remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra zram-generator-defaults
+dnf5 copr enable -y bieszczaders/kernel-cachyos-lto
+dnf5 copr enable -y bieszczaders/kernel-cachyos-addons
+rpm-ostree install kernel-cachyos-lto
+dnf5 -y install scx-scheds cachyos-settings uksmd
+systemctl enable scx.service
+systemctl enable uksmd.service
