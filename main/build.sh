5b7cff18b0d80d6e178b9672087cd49d0a59e1e1
XLion <xlion@xlion.tw>
2025-05-08 14:36:47 +0800

    KVM ptp

--- main/build.sh
+++ main/build.sh
@@ -11,18 +11,21 @@ echo "Creating symlinks to fix packages that install to /opt"
 mkdir -p "/var/opt"
 ln -s "/var/opt"  "/opt"
 
+# KVM PTP setup
+echo "ptp_kvm" | tee /etc/modules-load.d/ptp_kvm.conf
+echo 'OPTIONS="-s /dev/ptp0 -c CLOCK_REALTIME -O 0 -m"' | tee /etc/sysconfig/phc2sys
+
 curl -fsSl https://xlionjuan.github.io/ntpd-rs-repos/rpm/xlion-ntpd-rs-repo.repo | tee /etc/yum.repos.d/xlion-ntpd-rs-repo.repo
 
 dnf5 upgrade -y dnf5
 # Install
-dnf5 install -y 'dnf5-command(copr)' screen ntpd-rs sudo-rs vim htop wget
+dnf5 install -y 'dnf5-command(copr)' screen ntpd-rs sudo-rs vim htop wget linuxptp
 
 ## Use ntpd-rs to replace chronyd
 systemctl disable chronyd
 systemctl enable ntpd-rs
 
-# CachyOS
-dnf5 copr enable -y bieszczaders/kernel-cachyos-addons
-dnf5 -y install scx-scheds cachyos-settings uksmd
-systemctl enable scx.service
-#systemctl enable uksmd.service
+# KVM PTP setup
+echo "ptp_kvm" | tee /etc/modules-load.d/ptp_kvm.conf
+echo 'OPTIONS="-s /dev/ptp0 -c CLOCK_REALTIME -O 0 -m"' | tee /etc/sysconfig/phc2sys
+systemctl enable phc2sys.service
